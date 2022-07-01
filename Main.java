86783140c0ba0ce2c6c2484e97d016205d352584
Morandev <moranfcnddev@gmail.com>
2022-07-01 20:36:57 -0300

    add: saludar

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
-	public static void main( String[] args ) {
-		System.out.println("Hi World!");
+	public static void main( String[] args ) { 
+		saludar( "morandev" );
+	}
+
+	public static String saludar( String name ) {
+		return "Hi " + name + "!";
 	}
 
 }
